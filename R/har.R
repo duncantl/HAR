a2c07f56aba4fcf73f6d8ca43b4be6f9f10b9c33
Duncan Temple Lang <duncan@r-project.org>
2025-05-10 11:12:07 -0700

    down to .112 for readHAR(d = j) from 1.98. Factor of 17

--- R/har.R
+++ R/har.R
@@ -9,7 +9,7 @@ function(file, d = fromJSON(file))
 
 EntryFields = c(
                  "cache", "pageref", "request", "response", "startedDateTime", "time", "timings",
-                # following are not in all.
+                # following are not in every request
                 "_securityState", "serverIPAddress", "connection" 
               )
 
@@ -19,18 +19,26 @@ EntryFields = c(startedDateTime = "character", request = "list", response = "lis
                 )
 
 
-mkEntryDF =
+asDF1 =
+function(x)    
+{
+    structure(as.list(x), row.names = 1L, class = "data.frame")
+}
+
+
+mkEntryDF = mkEntryDF0 = mkEntryDF00 =
 function(x)
 {
     scalar = c("startedDateTime", "_securityState", "serverIPAddress", "connection", "pageref", "time")
     tmp = lapply(x[scalar], orNA)
+    names(tmp) = scalar
 
-    ans = as.data.frame(tmp)
-    names(ans) = scalar
+    #    ans = as.data.frame(tmp)
+    ans = asDF1(tmp)
 
     # timings
     if(length(x$timings))
-        ans[names(x$timings)] = as.data.frame(as.list(x$timings))
+        ans[names(x$timings)] = asDF1(as.list(x$timings)) # as.data.frame(as.list(x$timings))
     else
         ans = cbind(ans, data.frame("blocked" = NA,  "dns" = NA, connect = NA, "ssl" = NA, "send" = NA, "wait" = NA, "receive" = NA))
     
@@ -46,12 +54,46 @@ function(x)
     ans
 }
 
+
+
+
+mkEntryDF = mkEntryDF0 =
+function(x)
+{
+    scalar = c("startedDateTime", "_securityState", "serverIPAddress", "connection", "pageref", "time")
+    tmp = lapply(x[scalar], orNA)
+    names(tmp) = scalar
+
+    #    ans = as.data.frame(tmp)
+    ans = tmp
+
+    # timings
+    if(length(x$timings))
+        ans = c(ans, as.list(x$timings)) # as.data.frame(as.list(x$timings))
+    else
+        ans = c(ans, list("blocked" = NA,  "dns" = NA, connect = NA, "ssl" = NA, "send" = NA, "wait" = NA, "receive" = NA))
+    
+    # request, response, cache
+    # For now, ignore cache as I have that turned off.
+
+    r1 = mkRequest(x$request)
+    ans[names(r1)] = r1
+
+    r2 = mkResponse(x$response)
+    ans[names(r2)] = r2
+
+    # cbind(asDF1(ans), r1, r2)
+
+    ans
+}
+
+
 orNA =
 function(x)
     if(length(x) == 0) NA else x
 
 
-queryString =
+queryString = queryString0 =
     # convert the list of name value vectors into a single vector with the values
     # as the elements and the field name as the names() vector.
 function(x)
@@ -67,34 +109,87 @@ function(x)
     v =  unlist(x)
     i = seq(1, by = 2, length = length(x))
     structure(v[ i + 1L], names = v[i])
+    # Which is faster - structure() or ans = ; names(ans) = ; ans
+    #ans = v[ i + 1L]
+    #names(ans) = v[i]
+    #ans
+}
+
+queryString = 
+    # convert the list of name value vectors into a single vector with the values
+    # as the elements and the field name as the names() vector.
+function(x)
+{
+
+    # w/o this check for length(x), get warnings of the form
+    # In structure(v[i + 1L], names = v[i]) :
+    #  Calling 'structure(NULL, *)' is deprecated, as NULL cannot have attributes.
+    #  Consider 'structure(list(), *)' instead.
+    if(length(x) == 0)
+        return(character())
+    
+    tmp = matrix(unlist(x), , 2, byrow = TRUE)
+    z = tmp[,2]
+    names(z) = tmp[,1]
+    z
 }
 
 
+
+# This version is slower than the above.
+queryString.slower = 
+    # convert the list of name value vectors into a single vector with the values
+    # as the elements and the field name as the names() vector.
+function(x)
+{
+
+    # w/o this check for length(x), get warnings of the form
+    # In structure(v[i + 1L], names = v[i]) :
+    #  Calling 'structure(NULL, *)' is deprecated, as NULL cannot have attributes.
+    #  Consider 'structure(list(), *)' instead.
+    if(length(x) == 0)
+        return(character())
+    
+    tmp = matrix(unlist(x), , 2, byrow = TRUE)
+    structure(tmp[,2], names = tmp[,1])
+}
+
+
+
+# This version creates the entire list and then calls asDF1
+# Check gives same result
 mkRequest =
 function(x)
 {
     # scalars
     s = c("bodySize", "method", "url", "httpVersion", "headersSize")
-    ans = as.data.frame(lapply(x[s], orNA))
+    ans = lapply(x[s], orNA) 
 
     ans$requestHeaders = list(queryString(x$headers))
     ans$requestCookies = list(queryString(x$cookies))
     ans$queryString = list(queryString(x$queryString))
     ans$postData = list(queryString(x$postData))
-    ans
+
+    asDF1(ans)
+#    ans
 }
 
-mkResponse =
+
+
+mkResponse = 
 function(x)
 {
     # scalars
     s = c("status", "statusText", "httpVersion", "redirectURL", "headersSize", "bodySize")
-    ans = as.data.frame(lapply(x[s], orNA))
+    ans = lapply(x[s], orNA)
+    # added this when did not get the same answer as mkResponse0
+    names(ans) = s
+    
 
     ans$responseHeaders = list(queryString(x$headers))
     ans$responseCookies = list(queryString(x$cookies))
     ans$content = orNA(x$content$text)
     ans$mimeType = orNA(x$content$mimeType)
     
-    ans
+    asDF1(ans)
 }
